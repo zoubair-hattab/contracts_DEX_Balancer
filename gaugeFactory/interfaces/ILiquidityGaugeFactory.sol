7ed5a43cc4d2b178479507d29561b51f7914d682
zoubair.hattab <elhattabzoubair93@gmail.com>
2022-09-29 12:41:06 +0100

    ex

---
+++ gaugeFactory/interfaces/ILiquidityGaugeFactory.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "./ILiquidityGauge.sol";
+
+interface ILiquidityGaugeFactory {
+    /**
+     * @notice Returns true if `gauge` was created by this factory.
+     */
+    function isGaugeFromFactory(address gauge) external view returns (bool);
+}
